64f4e11b5c4998a968f20026a2ee9773b88fda19
Ashirwada Dayarathne <ashirwada@wso2s-MacBook-Pro.local>
2018-09-13 09:31:24 +0530

    Added a text box to give a name for the test confiiguration

--- components/ui-component/src/SpecificationSelectView.jsx
+++ components/ui-component/src/SpecificationSelectView.jsx
@@ -19,7 +19,7 @@
 import React from 'react';
 import AppHeader from "./partials/AppHeader";
 import AppBreadcrumbs from "./partials/AppBreadcrumbs";
-import {ListGroup, ListGroupItem, Glyphicon, Button} from 'react-bootstrap';
+import {ListGroup, ListGroupItem, FormControl, Button, Panel} from 'react-bootstrap';
 import {connect} from 'react-redux'
 import {addSpecification, toggleSpecification, clearSpecifications} from "./actions";
 import {Link} from 'react-router-dom'
@@ -69,10 +69,17 @@ class SpecificationSelectView extends React.Component {
         return (
             <div>
                 <h1>Available APIs in the AU Open Banking Specification</h1>
+                <Panel>
+                    <Panel.Heading>Name of the Test Configuration</Panel.Heading>
+                    <Panel.Body>
+                    <form>
+                        <FormControl type="text" value="" placeholder="Enter Name for the Text Configuration"/>
+                    </form>
+                    </Panel.Body>
+                </Panel>
                 <hr/>
-                <h4>Select the APIs to test</h4>
                 <ListGroup>
-                    {/* <ListGroupItem disabled><b>Available APIs</b></ListGroupItem> */}
+                    <ListGroupItem disabled><h4>Select the APIs to test</h4></ListGroupItem>
                     {Object.values(this.props.specifications.specs).map((spec) => {
                         return this.renderSpec(spec)
                     })}
