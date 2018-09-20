7a0b936ebd444549cc97addc4ce36274ee27d55d
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2018-09-20 11:47:47 +0530

    CORS interceptor added

---
+++ components/com.wso2.finance.open.banking.conformance.api/src/main/java/com/wso2/finance/open/banking/conformance/api/interceptors/CorsInterceptor.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package com.wso2.finance.open.banking.conformance.api.interceptors;
+
+import org.wso2.msf4j.Request;
+import org.wso2.msf4j.Response;
+import org.wso2.msf4j.interceptor.ResponseInterceptor;
+
+public class CorsInterceptor implements ResponseInterceptor {
+
+    @Override
+    public boolean interceptResponse(Request request, Response response) throws Exception {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, X-Requested-With, remember-me");
+
+        if (request.getHeader("Origin") != null && !request.getHeader("Origin").isEmpty()) {
+            response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        }
+        return true;
+    }
+}
