6fe0f8617bf35b27d8a9e8d3be90d163cc411fe2
Kaveen Rodrigo <u.k.k.rodrigo@gmail.com>
2018-09-21 11:22:45 +0530

    Update report on complete

--- components/com.wso2.finance.open.banking.conformance.test.core/src/main/java/com/wso2/finance/open/banking/conformance/test/core/runner/TestPlanRunnerInstance.java
+++ components/com.wso2.finance.open.banking.conformance.test.core/src/main/java/com/wso2/finance/open/banking/conformance/test/core/runner/TestPlanRunnerInstance.java
@@ -108,6 +108,7 @@ public class TestPlanRunnerInstance extends Thread{
         }
         this.status = Report.RUNNER_STATE.DONE;
         this.testPlan.setLastRun(new Date());
+        this.managerCallbacks.updateResult(this.buildReport());
         queueStopMessege();
         this.interrupt();
     }
